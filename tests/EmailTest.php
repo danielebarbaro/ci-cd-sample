fdd11cb804f3f580075d55c45eb9b30acf344f0f
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-06-16 20:18:15 +0200

    Test pipeline

---
+++ tests/EmailTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Danielebarbaro\CiCdSample\Tests;
+
+use Danielebarbaro\CiCdSample\Email;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmail(): void
+    {
+        $string = 'user@example.com';
+
+        $email = Email::fromString($string);
+
+        $this->assertSame($string, $email->asString());
+    }
+
+    public function testCannotBeCreatedFromInvalidEmail(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+}
